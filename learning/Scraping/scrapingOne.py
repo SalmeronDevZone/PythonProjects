6f986055ae7348864d1c752ed2a918bb862bf947
SalmeronDevZone <162796372+SalmeronDevZone@users.noreply.github.com>
2024-04-23 11:43:38 +0200

    Create scrapingOne.py

---
+++ learning/Scraping/scrapingOne.py
@@ -0,0 +1,25 @@
+import requests 
+from bs4 import BeautifulSoup
+
+
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+
+#print(soup.prettify())
+
+box = soup.find('article', class_ = 'main-article') #Ojo alguien de class.
+
+title = box.find('h1').get_text()
+
+transcript = box.find('div', class_ = 'full-script').get_text(strip=True, separator = '')
+
+print(title)
+print(transcript)
+
+
+with open(f'{title}', 'w') as file:
+    file.write(transcript)
