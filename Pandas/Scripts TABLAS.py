5f279e325b85289610b52a871a31b3e3b39f3932
Jesús <162796372+SalmeronDevZone@users.noreply.github.com>
2025-09-18 12:00:36 +0200

    firstDayBasicScripts

---
+++ Pandas/Scripts TABLAS.py
@@ -0,0 +1,16 @@
+
+
+data = [[1, 'Joe', 70000, 3], [2, 'Henry', 80000, 4], [3, 'Sam', 60000, None], [4, 'Max', 90000, None]]
+employee = pd.DataFrame(data, columns=['id', 'name', 'salary', 'managerId']).astype({'id':'Int64', 'name':'object', 'salary':'Int64', 'managerId':'Int64'})
+
+
+
+data = [[1, 'a@b.com'], [2, 'c@d.com'], [3, 'a@b.com']]
+person = pd.DataFrame(data, columns=['id', 'email']).astype({'id':'Int64', 'email':'object'})
+
+
+
+data = [[1, 'Joe'], [2, 'Henry'], [3, 'Sam'], [4, 'Max']]
+customers = pd.DataFrame(data, columns=['id', 'name']).astype({'id':'Int64', 'name':'object'})
+data = [[1, 3], [2, 1]]
+orders = pd.DataFrame(data, columns=['id', 'customerId']).astype({'id':'Int64', 'customerId':'Int64'})
